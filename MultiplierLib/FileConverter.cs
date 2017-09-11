6d615a2267f69bddcfccacfd9ef9a23a76583bc3
Amin Zare <amin.zare.spo16@ecusolna.onmicrosoft.com>
2017-09-11 13:48:06 +0200

    Convert test

---
+++ MultiplierLib/FileConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplierLib
+{
+	public class FileConverter
+	{
+		public string Transform(string row)
+		{
+			string[] toknes = row.Split(';');
+			var firstName = toknes[0].ToUpper();
+			var lastName = toknes[1].ToUpper();
+			var years = DateTime.Now.Year - int.Parse(toknes[2]);
+			var message = $"{firstName} {lastName} ÄR { years} ÅR GAMMAL.";
+			return message;
+		}
+		public string Transform1(string row)
+		{
+			string[] toknes = row.Split(';');
+			var firstName = toknes[0].ToUpper();
+			var lastName = toknes[1].ToUpper();
+			var years = DateTime.Now.Year - int.Parse(toknes[2]);
+			var message = $"{firstName} {lastName} ÄR { years} ÅR GAMMAL.";
+			return message;
+		}
+	}
+}
