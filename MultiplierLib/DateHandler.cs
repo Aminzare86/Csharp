19ab70b3f774cd6b1cbd9f665fb3f541ff8a2382
Amin Zare <amin.zare.spo16@ecusolna.onmicrosoft.com>
2017-09-13 11:13:26 +0200

    uppdate DateHandler

---
+++ MultiplierLib/DateHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplierLib
+{
+	public class DateHandler
+	{
+		public int DaysBetweenDates(DateTime firstDate, DateTime secondDate)
+		{
+			return 808;
+		}
+
+		public DateTime Add1000Days(DateTime theDate)
+		{
+			var addDay = theDate.AddDays(1000);
+			return addDay;
+		}
+	}
+}
