db112550e84dba58682359f965571acb35483957
Amin Zare <amin.zare.spo16@ecusolna.onmicrosoft.com>
2017-10-04 14:56:20 +0200

    last chand pershon nr whit do while

--- RäknaSistaSifro/Program.cs
+++ RäknaSistaSifro/Program.cs
@@ -10,37 +10,54 @@ namespace RäknaSistaSifro
 	{
 		static void Main(string[] args)
 		{
+			var finaly = 0;
+			do
+			{
 			Console.WriteLine("Inter your person nr");
 			var personNr =Console.ReadLine();
 			int[] arr = { 2, 1, 2, 1, 2, 1, 2, 1, 2 };
 			var peronArr = personNr.ToArray().Select(ch => int.Parse(ch.ToString())).ToArray();
-			int [] x = { };
 			var total=0;
 			var sut=0;
-			var finaly = 0;
 			for (int i = 0; i <= 8; i++)
 			{
 				 sut = (peronArr[i]) * (arr[i]);
 				if (sut > 9)
-				{
-					
+				{					
 					var sutToChar = Convert.ToString(sut);
 					var sutToIntArray = sutToChar.ToArray().Select(ch => int.Parse(ch.ToString())).ToArray();
 					sut = (sutToIntArray[1]) + (sutToIntArray[0]);
-					total += sut;
-					
+					total += sut;					
 				}
 				else
 				{
 					total += sut;
 				}
-				finaly = 30 - total;
-			}
-
+				if (total <= 30)
+				{
+					finaly = 30 - total;
+				}
+				else if (total > 30 && total <= 40)
+				{
+					finaly = 40 - total;
+				}
+				else if (total > 40 && total <= 50)
+				{
+					finaly = 50 - total;
+				}
+				else if (total > 50 && total <= 60)
+				{
+					finaly = 60 - total;
+				}
+				else if (total > 60 && total <= 70)
+				{
+					finaly = 70 - total;
+				}
+			}	
 			Console.WriteLine("its your Answer {0}",finaly);
-			Console.ReadLine();
 
-
+			} while (finaly>=0);
+			
 		}
 	}
 }
